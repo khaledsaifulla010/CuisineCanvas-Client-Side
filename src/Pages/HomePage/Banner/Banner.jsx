396e89638c808fd4a4c402cd1922e7a69ae23f70
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-29 11:25:59 +0600

    added image in carousel in banner

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,21 +1,30 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
+import slide1 from "../../../assets/banner/slide1.jpg";
+import slide2 from "../../../assets/banner/slide2.jpg";
+import slide3 from "../../../assets/banner/slide3.jpg";
+import slide4 from "../../../assets/banner/slide4.jpg";
+import slide5 from "../../../assets/banner/slide5.jpg";
+
 const Banner = () => {
   return (
     <div>
       <Carousel>
         <div>
-          <img src="assets/1.jpeg" />
-          <p className="legend">Legend 1</p>
+          <img src={slide1} alt="" />
         </div>
         <div>
-          <img src="assets/2.jpeg" />
-          <p className="legend">Legend 2</p>
+          <img src={slide2} alt="" />
         </div>
         <div>
-          <img src="assets/3.jpeg" />
-          <p className="legend">Legend 3</p>
+          <img src={slide3} alt="" />
+        </div>
+        <div>
+          <img src={slide4} alt="" />
+        </div>
+        <div>
+          <img src={slide5} alt="" />
         </div>
       </Carousel>
     </div>

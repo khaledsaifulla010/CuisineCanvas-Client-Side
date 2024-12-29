cb4156d7e0a032029a31312bd326c1af6dcd5c97
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-29 12:38:54 +0600

    added online food slide section in home

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import OnlineOrder from "../OnlineOrder/OnlineOrder";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <OnlineOrder></OnlineOrder>
     </div>
   );
 };

4fa54d12567263ea33afe68bd96d7631af584a74
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-29 09:42:16 +0600

    added footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="max-w-[1450px] mx-auto">
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };

b29696fe18dcde1540598200e75428d662d07bab
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-29 10:01:06 +0600

    added logo & name in navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-
+import logo from "../../../assets/images/logo.png";
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="flex items-center gap-1">
+            <img className="w-8" src={logo} />
+            <h1 className="text-3xl font-bold">CuisineCanvas</h1>
+          </div>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
-
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Shared/Footer/Footer";
+import Navbar from "../components/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div className="max-w-[1450px] mx-auto">
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>

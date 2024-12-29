a8f8791e2531f95d34b1d53a7d07dc3639dc865c
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-29 15:26:03 +0600

    added button in popular menu section

--- src/Pages/HomePage/PopularMenus/PopularMenus.jsx
+++ src/Pages/HomePage/PopularMenus/PopularMenus.jsx
@@ -13,17 +13,23 @@ const PopularMenus = () => {
   });
 
   return (
-    <div>
+    <div className="mb-12">
       <SectionTitle
         subHeading={"Check It Out"}
         heading={"OUR POPULAR MENUS"}
       ></SectionTitle>
-      <div className="flex flex-wrap justify-between gap-6 mb-24">
+      <div className="flex flex-wrap justify-between gap-6 ">
         {menus.map((menu) => (
           <div key={menu._id} className="w-[48%]">
             <PopularMenusCard menu={menu} />
           </div>
         ))}
+      </div>
+      <div className="mb-12 text-center">
+        <button className="relative text-lg font-bold uppercase text-gray-800 tracking-wide">
+          View Full Menu
+          <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-24 border-b-2 border-gray-800 rounded-full"></div>
+        </button>
       </div>
     </div>
   );

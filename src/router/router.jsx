c94fc2e1eb46635dd7594fdae10751ae79bde025
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-30 09:20:19 +0600

    added our menu page and its routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../Pages/HomePage/Home/Home";
+import OurMenu from "../Pages/OurMenuPage/OurMenu/OurMenu";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/ourMenu",
+        element: <OurMenu></OurMenu>,
       },
     ],
   },

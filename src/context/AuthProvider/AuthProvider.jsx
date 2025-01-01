8fc007582dc25eeaff6e410896a14d0fee6a7c1f
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-01 09:42:06 +0600

    added onAuthStateChanged as a ovserver for getting current user

--- src/context/AuthProvider/AuthProvider.jsx
+++ src/context/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import AuthContext from "../AuthContext/AuthContext";
-
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 const AuthProvider = ({ children }) => {
-  const user = { name: "Khaled" };
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log("current User", currentUser);
+    });
+    return () => {
+      return unsubscribe;
+    };
+  }, []);
 
   const authInfo = {
     user,
+    loading,
   };
 
   return (

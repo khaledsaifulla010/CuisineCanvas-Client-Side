7053146f012c5fff6d34b5247437223db7469502
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-01 09:47:06 +0600

    added create user method using email & password

--- src/context/AuthProvider/AuthProvider.jsx
+++ src/context/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import AuthContext from "../AuthContext/AuthContext";
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  // Create User //
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -20,6 +30,7 @@ const AuthProvider = ({ children }) => {
   const authInfo = {
     user,
     loading,
+    createUser,
   };
 
   return (

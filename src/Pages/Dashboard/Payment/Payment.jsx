a5990a473994fd85fb9a89c3a08056e3f4f38417
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-13 11:12:36 +0600

    install stripe & use card elements

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,18 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
+// Add Publishable Key
+const stripePromise = loadStripe()
 
 const Payment = () => {
     return (
+      <div>
+        <h1 className="text-center text-5xl font-bold">Payment</h1>
+
         <div>
-            Payment
+            <Elements stripe={stripePromise}></Elements>
         </div>
+      </div>
     );
 };
 
